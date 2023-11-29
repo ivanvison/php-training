820eae8f651ab96365cf7aaa30d1fa5cb58bc3c7
Ivan <ivanvison@gmail.com>
2023-11-29 13:26:15 -0500

    Testing Copilot in this PHP file.

--- index.php
+++ index.php
@@ -5,5 +5,15 @@
     </head>
     <body>
         <?php echo '<p>Hello World!!</p>'; ?>
+        <?php echo '<p>PHP is working</p>'; ?>
+        
+        <form action="index.php" method="post">
+            <input type="text" name="num1" placeholder="Number 1">
+            <input type="text" name="num2" placeholder="Number 2">
+            <input type="submit">
+        </form>
+        Answer: <?php echo $_POST["num1"] + $_POST["num2"] ?>
+
+
     </body>
 </html>
